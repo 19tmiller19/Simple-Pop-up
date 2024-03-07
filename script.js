4c64b854d3a83e20cac9ea7bfadd2dec53f91eda
Troy <98496851+19tmiller19@users.noreply.github.com>
2024-03-07 13:59:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");//when function is called athe pop up shows
+}
+function closePopup(){
+    popup.classList.remove("open-popup");/// when function is called the pop up closes
+}
